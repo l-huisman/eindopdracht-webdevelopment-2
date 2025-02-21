1858af1f56eb05f16e7e5f82b3b9bbf1ad5c7b00
Luke Huisman <huisman.l@yahoo.com>
2025-02-21 13:46:14 +0100

    Enhance RegisterView with terms acceptance checkbox and validation

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -7,14 +7,16 @@ const email = ref<string>('');
 const password = ref<string>('');
 const confirmPassword = ref<string>('');
 const rememberMe = ref<boolean>(false);
+const acceptTerms = ref<boolean>(false);
 
 const register = () => {
   const usernameError = document.getElementById('usernameError') as HTMLLabelElement;
   const emailError = document.getElementById('emailError') as HTMLLabelElement;
   const passwordError = document.getElementById('passwordError') as HTMLLabelElement;
   const confirmPasswordError = document.getElementById('confirmPasswordError') as HTMLLabelElement;
+  const termsError = document.getElementById('termsError') as HTMLLabelElement;
 
-  usernameError.textContent = emailError.textContent = passwordError.textContent = confirmPasswordError.textContent = '';
+  usernameError.textContent = emailError.textContent = passwordError.textContent = confirmPasswordError.textContent = termsError.textContent = '';
 
   if (!username.value) {
     usernameError.textContent = 'Please enter your username';
@@ -39,22 +41,20 @@ const register = () => {
     return;
   }
 
+  if (!acceptTerms.value) {
+    termsError.textContent = 'You must accept the terms and conditions';
+    return;
+  }
+
   if (rememberMe.value) {
-    localStorage.setItem('email', email.value);
     localStorage.setItem('password', password.value);
-
-    // Save the username to local storage
     localStorage.setItem('username', username.value);
-
-    // Redirect to the calendar page
     router.push('/calendar');
     return;
   }
 
-  // Redirect to the calendar page
   router.push('/login');
 }
-
 
 const showPassword = (event: Event) => {
   const button = event.target as HTMLButtonElement;
@@ -72,7 +72,8 @@ const showPassword = (event: Event) => {
 
 <template>
   <main class="h-svh flex justify-center items-center">
-    <div class="w-sm px-10 py-5 flex flex-col justify-center items-center gap-5 bg-gray-50 drop-shadow-sm">
+    <div
+      class="w-sm px-10 py-5 flex flex-col justify-center items-center gap-5 bg-gray-50 drop-shadow-sm">
       <h1 class="text-2xl">Register</h1>
       <form class="flex flex-col gap-3">
         <div class="flex flex-col gap-1">
@@ -89,7 +90,9 @@ const showPassword = (event: Event) => {
           <label class="text-sm" for="password">Password</label>
           <div class="border-2 border-gray-300 flex items-center gap-0.5">
             <input v-model="password" class="p-2 flex-1" type="password" id="password"/>
-            <button class="text-sm h-10 w-16 hover:bg-gray-300 bg-gray-200 active:bg-gray-400" @click.prevent="showPassword($event)">Show</button>
+            <button class="text-sm h-10 w-16 hover:bg-gray-300 bg-gray-200 active:bg-gray-400"
+                    @click.prevent="showPassword($event)">Show
+            </button>
           </div>
           <label id="passwordError" class="text-sm text-red-600" for="passwordError"></label>
         </div>
@@ -97,16 +100,28 @@ const showPassword = (event: Event) => {
           <label class="text-sm" for="confirm-password">Confirm Password</label>
           <div class="border-2 border-gray-300 flex items-center gap-0.5">
             <input v-model="confirmPassword" class="p-2" type="password" id="confirm-password"/>
-            <button class="text-sm h-10 w-16 hover:bg-gray-300 bg-gray-200 active:bg-gray-400" @click.prevent="showPassword($event)">Show</button>
+            <button class="text-sm h-10 w-16 hover:bg-gray-300 bg-gray-200 active:bg-gray-400"
+                    @click.prevent="showPassword($event)">Show
+            </button>
           </div>
-          <label id="confirmPasswordError" class="text-sm text-red-600" for="confirmPasswordError"></label>
+          <label id="confirmPasswordError" class="text-sm text-red-600"
+                 for="confirmPasswordError"></label>
         </div>
-        <div class="flex items-center gap-2 mt-1">
+        <div>
+          <div class="flex items-center gap-2">
+            <input v-model="acceptTerms" type="checkbox" id="accept-terms"/>
+            <label class="text-sm" for="accept-terms">I accept to the <router-link class="text-cyan-900 underline" to="/terms">terms and conditions</router-link></label>
+          </div>
+          <label id="termsError" class="text-sm text-red-600" for="termsError"></label>
+        </div>
+        <div class="flex items-center gap-2">
           <input v-model="rememberMe" type="checkbox" id="remember-me"/>
           <label class="text-sm" for="remember-me">Remember me</label>
         </div>
-        <button @click.prevent="register" class="my-2 bg-cyan-900 py-2.5 text-white">Register</button>
-        <router-link to="/login" class="text-center text-cyan-900 my-2">Already have a password?</router-link>
+        <button @click.prevent="register" class="my-2 bg-cyan-900 py-2.5 text-white">Register
+        </button>
+        <router-link to="/login" class="text-center text-cyan-900 my-2">Already have a password?
+        </router-link>
       </form>
     </div>
   </main>
