0e5041e65db28c56eb19381e4a23c078ade7c1a6
Luke Huisman <huisman.l@yahoo.com>
2025-03-03 22:20:58 +0100

    Update NavigationBar component with new links for Login, Register, Admin, Calendar, and Logout; enhance styling for special button

--- src/views/NavigationBar.vue
+++ src/views/NavigationBar.vue
@@ -6,7 +6,13 @@
   <header class="flex justify-between items-center px-10 py-5 bg-gray-50 drop-shadow-sm">
     <h1 class="text-2xl">Calendar App</h1>
     <nav>
-      <router-link class="router-link" to="/get-started">Get Started For Free</router-link>
+      <router-link class="router-link" to="/login">Login</router-link>
+      <router-link class="router-link" to="/get-started">Register</router-link>
+      <router-link class="router-link" to="/admin-dashboard">Admin</router-link>
+      <router-link class="router-link" to="/calendar">Calendar</router-link>
+      <router-link class="router-link" to="/logout">Logout</router-link>
+
+      <router-link class="special-button" to="/get-started">Get Started For Free</router-link>
     </nav>
   </header>
 </template>
@@ -15,6 +21,14 @@
 .router-link {
   border: var(--color-cyan-900) 1px solid;
   text-decoration: none;
+  padding: 0.5rem 1rem;
+  margin-left: 1rem;
+}
+
+.special-button {
+  background-color: var(--color-cyan-900);
+  color: white;
+  border: none;
   padding: 0.5rem 1rem;
   margin-left: 1rem;
 }
